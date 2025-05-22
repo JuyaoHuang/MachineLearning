9b39d6bf9b865f22994410fe3e5c9ef2a290b79b
Juyao Huang <tinghua5226@gamil.com>
2025-05-22 21:53:58 +0800

    This is an example of trainning model

--- ExampleWithAutoMPG.py
+++ ExampleWithAutoMPG.py
@@ -21,5 +21,22 @@ dataset['Japan'] = (origin == 3)*1.0
 print(dataset)
 
 # 为了评估模型，创建训练集和测试集
+# frac用于从原始数据集中抽取百分之多少的数据,random_state用于确保运行代码时抽样的结果是相同的（即随机性是可重复的）
+train_dataset = dataset.sample(frac=0.8,random_state=0)
+test_dataset = dataset.drop(train_dataset.index)
+# 计算各特征的统计值
+train_stats = train_dataset.describe()
+train_stats.pop("MPG")
+train_stats = train_stats.transpose()
+
+# 分别将训练集和测试集的训练特征和标签分离，标签是模型需要预测的值
+train_labels = train_dataset.pop('MPG')
+test_labels = test_dataset.pop('MPG')
+
+# 归一化处理 之后使用此归一化的数据来训练模型
+def norm(x):
+    return (x-train_stats['mean']) / train_stats['std']
+normed_train_data = norm(train_dataset)
+normed_test_data = norm(test_dataset)
 
 
